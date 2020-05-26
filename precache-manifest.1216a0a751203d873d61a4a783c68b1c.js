aff629a8d83091b3f724f54f9b1f95ea715bd249
Harshit Chouhan <harshitsinghchouhan2018@gmail.com>
2020-05-26 11:45:52 +0530

    Updates

---
+++ precache-manifest.1216a0a751203d873d61a4a783c68b1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7137f4e5e832ffd74edc44199ab6e638",
+    "url": "/colorApp/index.html"
+  },
+  {
+    "revision": "fa97962fc5b115de4ab1",
+    "url": "/colorApp/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "7aca75d70087857c1c70",
+    "url": "/colorApp/static/css/main.561cb46c.chunk.css"
+  },
+  {
+    "revision": "fa97962fc5b115de4ab1",
+    "url": "/colorApp/static/js/2.9740c39c.chunk.js"
+  },
+  {
+    "revision": "fe2f19c5d5965d51615a596cf8d07fe9",
+    "url": "/colorApp/static/js/2.9740c39c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aca75d70087857c1c70",
+    "url": "/colorApp/static/js/main.05522fed.chunk.js"
+  },
+  {
+    "revision": "e70d4440c67c8372fd6d",
+    "url": "/colorApp/static/js/runtime-main.40397a8f.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/colorApp/static/media/bg.77428cd4.svg"
+  }
+]);
